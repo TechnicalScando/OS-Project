c6551146db100a4699d2b3d72fe4922b88eecc4a
scando <kikz270@gmail.com>
2025-01-22 11:21:19 -0600

    first commit

---
+++ OsProject.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Main function - entry point of the program
+int main() {
+    // Output a simple message
+    std::cout << "Hello, World!" << std::endl;
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
